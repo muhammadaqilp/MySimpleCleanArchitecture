d5640d2e524df833db1c8b194a2c65bfcc60efeb
muhammadaqilp <muhammadpratama1816@gmail.com>
2022-03-06 10:37:26 +0700

    init

---
+++ app/src/main/java/com/example/mysimplecleanarchitecture/data/MessageDataSource.kt
@@ -0,0 +1,8 @@
+package com.example.mysimplecleanarchitecture.data
+
+import com.example.mysimplecleanarchitecture.domain.MessageEntity
+
+class MessageDataSource : IMessageDataSource {
+    override fun getMessageFromSource(name: String): MessageEntity =
+        MessageEntity("Hello $name! Welcome to Clean Architecture")
+}
---
+++ app/src/test/java/com/example/mysimplecleanarchitecture/domain/MessageUseCaseTest.kt
@@ -0,0 +1,40 @@
+package com.example.mysimplecleanarchitecture.domain
+
+import com.example.mysimplecleanarchitecture.data.MessageRepository
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MessageUseCaseTest {
+
+    private lateinit var messageUseCase: MessageUseCase
+
+    @Mock
+    private lateinit var messageRepository: MessageRepository
+
+    @Before
+    fun setUp() {
+        messageUseCase = MessageInteractor(messageRepository)
+        val dummyMessage = MessageEntity("Hello $NAME Welcome to Clean Architecture")
+        `when`(messageRepository.getWelcomeMessage(NAME)).thenReturn(dummyMessage)
+    }
+
+    @Test
+    fun `should get data from repository`() {
+        val message = messageUseCase.getMessage(NAME)
+
+        verify(messageRepository).getWelcomeMessage(NAME)
+        verifyNoMoreInteractions(messageRepository)
+        assertEquals("Hello $NAME Welcome to Clean Architecture", message.welcomeMessage)
+    }
+
+    companion object {
+        const val NAME = "Aqil"
+    }
+
+}
